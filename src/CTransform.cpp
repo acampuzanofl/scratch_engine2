ab00b7d8ed9942bae749ec6ca11bd3a81d2c915d
Your Name <you@example.com>
2023-04-29 06:09:09 +0000

    noted a bug with velocity decay

--- src/CTransform.cpp
+++ src/CTransform.cpp
@@ -1,6 +1,7 @@
 #include "CTransform.hpp"
 
 #include <SFML/System/Vector2.hpp>
+#include <cassert>
 
 #include "Component.hpp"
 #include "Debug.hpp"
@@ -33,6 +34,15 @@ const sf::Vector2f& CTransform::GetVelocity() const { return velocity; }
 void CTransform::SetWeight(float weight) { this->weight = weight; }
 void CTransform::Update(float dt) {
   position += velocity * dt;
+
+  /**
+   * TODO: if weight is too big velcocity can be negative
+   * this results in undefined behavior. For now, as long as
+   * weight is < 60, it seems to be fine. Turns out the behavior
+   * causes the position/velocity to oscilate between positive and negative
+   * this needs to be fixed properly.
+   */
+  assert(weight < 60);
   velocity *= 1.f - weight * dt;
 }
 void CTransform::SetMoveSpeed(int moveSpeed) { this->moveSpeed = moveSpeed; }
