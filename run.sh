f712c18944a20ace554077823230a961da739b92
Your Name <you@example.com>
2025-02-13 22:16:52 +0900

    updated run script to habdle debug running

--- run.sh
+++ run.sh
@@ -1,6 +1,22 @@
-#!/bin/sh
-pushd build/ > /dev/null
-cmake ..
-make 
-./scratch_engine2
-popd > /dev/null
+#!/bin/bash
+
+Run(){
+    pushd build/ > /dev/null
+    cmake ..
+    make 
+    ./scratch_engine2
+    popd > /dev/null
+}
+
+RunDebug(){
+    pushd build/ > /dev/null
+    cmake ..
+    make 
+    lldb ./scratch_engine2
+    popd > /dev/null
+}
+
+case $1 in 
+    debug  ) RunDebug   ;;
+    *      ) Run        ;;
+esac
